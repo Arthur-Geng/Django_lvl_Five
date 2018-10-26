19a33fc7c3f879441509f3c51dc0cb330c352bfa
ZloyEntrery <volodymyr_entrery@ukr.net>
2018-10-26 19:56:15 +0300

    1

--- Application/views.py
+++ Application/views.py
@@ -17,13 +17,13 @@ class SchoolDetailView(DetailView):
     template_name = 'Application/school_detail.html'
 
 # # Create your views here.
-# class IndexView(TemplateView):
-#     template_name = 'basic_app/index.html'
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['inject'] = 'injection'
-#         return context
+class IndexView(TemplateView):
+    template_name = 'basic_app/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['inject'] = 'injection'
+        return context
 
 
 
